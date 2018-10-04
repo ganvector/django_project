865461d7241a577dd82e7222f2198ccb9aca5377
milliardo <vitor.gan21@gmail.com>
2018-10-04 13:23:17 -0300

    Criados dummies de postagens para realizar testes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+postagens = [
+    {
+        'autor': 'Vitor Nascimento',
+        'titulo': 'Blog Post 1',
+        'conteudo': 'Conteudo do primeiro post',
+        'data_postagem': '4 de outubro de 2018'
+    },
+    {
+        'autor': 'Ninguem',
+        'titulo': 'Blog Post 2',
+        'conteudo': 'Conteudo do segundo post',
+        'data_postagem': '5 de outubro de 2018'
+    },
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': postagens
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
