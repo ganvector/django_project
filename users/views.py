3e652e9d64ec4d5b7dfa88cbf27ec22488ecea64
milliardo <vitor.gan21@gmail.com>
2018-10-12 19:34:24 -0300

    Revert "Revert "Criada classe formulario""
    This reverts commit e3b3bebefb414184e2e1cbcb9afb728786df2005.

--- users/views.py
+++ users/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from.formulario import UserRegisterForm
 
 def registro(request):
     if request.method == 'POST':
-        formulario = UserCreationForm(request.POST)
+        formulario = UserRegisterForm(request.POST)
         if formulario.is_valid():
             formulario.save()
             username = formulario.cleaned_data.get('username')
             messages.success(request, f'Parabens {username}! Conta criada com sucesso!')
             return redirect('blog-home')
     else:
-        formulario = UserCreationForm()
+        formulario = UserRegisterForm()
     return render(request, 'users/registro.html', {'form': formulario})
