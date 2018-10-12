832c3f78e44c1f7c73f4461ad0b122c43c43199e
milliardo <vitor.gan21@gmail.com>
2018-10-12 19:10:28 -0300

    Adicionado check de validação e cadastro

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def registro(request):
     if request.method == 'POST':
         formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            username = formulario.cleaned_data.get('username')
+            messages.success(request, f'Parabens {username}! Conta criada com sucesso!')
+            return redirect('blog-home')
     else:
         formulario = UserCreationForm()
     return render(request, 'users/registro.html', {'form': formulario})
