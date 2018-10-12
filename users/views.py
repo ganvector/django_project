949434b67329ab6e098ab85c8270414422eaa76e
milliardo <vitor.gan21@gmail.com>
2018-10-12 18:42:17 -0300

    Atualizado views do user
    Atualizado para que possa realizar alguma ação com os dados do formulario

--- users/views.py
+++ users/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 def registro(request):
-    formulario = UserCreationForm()
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+    else:
+        formulario = UserCreationForm()
     return render(request, 'users/registro.html', {'form': formulario})
