659653b39496cc980d5b28ff8be9a20aa648aebb
milliardo <vitor.gan21@gmail.com>
2018-10-15 17:27:15 -0300

    Update users/views.py
    Apos cadastro agora é redirecionado para a pagina de login

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def registro(request):
         if formulario.is_valid():
             formulario.save()
             username = formulario.cleaned_data.get('username')
-            messages.success(request, f'Parabens {username}! Conta criada com sucesso!')
-            return redirect('blog-home')
+            messages.success(request, f'Sua conta foi criada com sucesso! Voce agora pode entrar.')
+            return redirect('login')
     else:
         formulario = UserRegisterForm()
     return render(request, 'users/registro.html', {'form': formulario})
