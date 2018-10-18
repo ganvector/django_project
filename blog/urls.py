eb29538e9c77b9f4a2ac9f9b4b7875d4f568023a
milliardo <vitor.gan21@gmail.com>
2018-10-17 22:23:29 -0300

    Update blog/urls.py
    add caminha da url para a atualizacao e a exclusão

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
     path('post/novo/', PostCreateView.as_view(), name = 'post-create'),
+    path('post/<int:pk>/atualiza/', PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/deletar/', PostDeleteView.as_view(), name = 'post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
