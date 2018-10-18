df5ea03858b021273b7fee092aebc866b34514e4
milliardo <vitor.gan21@gmail.com>
2018-10-17 23:54:12 -0300

    Update blog/urls.py
    Criado rota para postagens de um usuario

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name = 'user-post'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
     path('post/novo/', PostCreateView.as_view(), name = 'post-create'),
     path('post/<int:pk>/atualiza/', PostUpdateView.as_view(), name = 'post-update'),
